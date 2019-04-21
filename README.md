de68dfae7cb30f6613186177d7f5d7d62bed7c7f
Lucas Pentinsaari <kyusaki1@gmail.com>
2019-04-22 02:10:54 +0300

    Fixed toolbar with menu, yet to fix working menu items

--- README.md
+++ README.md
@@ -34,6 +34,6 @@ A school assignment project for a Mobile App Development course
 
 
 # Current status
-  - Assignment 1 completed :heavy_check_mark:
-  - Assignment 2 completed :heavy_check_mark:
-  - Assignment 3 started
+  - [x] Assignment 1 completed ​
+  - [x] Assignment 2 completed 
+  - [ ] Assignment 3 started
--- app/src/main/java/moe/dreameh/assignment1/MainActivity.kt
+++ app/src/main/java/moe/dreameh/assignment1/MainActivity.kt
@@ -1,9 +1,12 @@
 package moe.dreameh.assignment1
 
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
-
+import androidx.appcompat.widget.Toolbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +14,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val tb: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(tb)
+        when (resources?.configuration?.orientation) {
+            Configuration.ORIENTATION_PORTRAIT -> {
+
+                supportActionBar?.show()
+            }
+            else -> supportActionBar?.hide()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        return true
     }
 }
